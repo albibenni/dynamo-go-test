9ec60ae19ea7d79c579048c2f795bf748ba16909
Alberto Benatti <albi.benni8@gmail.com>
2024-07-23 21:02:08 +0200

    Create database.go

---
+++ database/database.go
@@ -0,0 +1,110 @@
+package database
+
+import (
+	"fmt"
+
+	"github.com/albibenni/dynamo-go-test/types"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/dynamodb"
+	"github.com/aws/aws-sdk-go/service/dynamodb/dynamodbattribute"
+)
+
+const (
+	TABLE_NAME = "userTable"
+)
+
+type UserStore interface {
+	DoesUserExist(username string) (bool, error)
+	InsertUser(user types.User) error
+	GetUser(username string) (types.User, error)
+}
+
+type DynamoDBClient struct {
+	databaseStore *dynamodb.DynamoDB
+}
+
+func NewDynamoDBClient() DynamoDBClient {
+	dbSession := session.Must(session.NewSession())
+	db := dynamodb.New(dbSession, aws.NewConfig().WithEndpoint("http://localhost:8000"))
+
+	return DynamoDBClient{
+		databaseStore: db,
+	}
+}
+
+// Does this user exists?
+// How do I insert a new record into DynamoDB
+
+func (u DynamoDBClient) DoesUserExist(username string) (bool, error) {
+	result, err := u.databaseStore.GetItem(&dynamodb.GetItemInput{
+		TableName: aws.String(TABLE_NAME),
+		Key: map[string]*dynamodb.AttributeValue{
+			"username": {
+				S: aws.String(username),
+			},
+		},
+	})
+
+	if err != nil {
+		return true, err
+	}
+
+	if result.Item == nil {
+		return false, nil
+	}
+
+	return true, nil
+}
+
+func (u DynamoDBClient) InsertUser(user types.User) error {
+	// assemble the item
+	item := &dynamodb.PutItemInput{
+		TableName: aws.String(TABLE_NAME),
+		Item: map[string]*dynamodb.AttributeValue{
+			"username": {
+				S: aws.String(user.Username),
+			},
+			"email": {
+				S: aws.String(user.Email),
+			},
+		},
+	}
+
+	_, err := u.databaseStore.PutItem(item)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (u DynamoDBClient) GetUser(username string) (types.User, error) {
+	var user types.User
+
+	result, err := u.databaseStore.GetItem(&dynamodb.GetItemInput{
+		TableName: aws.String(TABLE_NAME),
+		Key: map[string]*dynamodb.AttributeValue{
+			"username": {
+				S: aws.String(username),
+				//! todo pk + sk
+			},
+		},
+	})
+
+	if err != nil {
+		return user, err
+	}
+
+	if result.Item == nil {
+		return user, fmt.Errorf("user not found")
+	}
+
+	err = dynamodbattribute.UnmarshalMap(result.Item, &user)
+	if err != nil {
+		return user, err
+	}
+
+	return user, nil
+
+}
