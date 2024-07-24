d69987d2cd4189db6abc595cbc0ccc7444830f3e
Alberto Benatti <albi.benni8@gmail.com>
2024-07-24 21:40:17 +0200

    Update dynamo_test.go

--- tests/dynamo_test.go
+++ tests/dynamo_test.go
@@ -1,17 +1,35 @@
 package main
 
 import (
-	"github.com/albibenni/dynamo-go-test/database"
 	"testing"
+
+	"github.com/albibenni/dynamo-go-test/database"
 )
 
-func Test(t *testing.T) {
-	client, err := database.NewDynamoDBClient()
-	if err != nil {
-		t.Error("Error creating database")
+const (
+	TABLE_NAME = "test-db"
+)
+
+func TestPort(t *testing.T) {
+	_, port := database.CreateLocalClient(8000)
+	if port != 8000 {
+		t.Error("Error, wrong port")
 	}
-	_, err2 := client.GetUser("Nope")
-	if err2 == nil {
-		t.Error("Error, no user yet imported")
+}
+
+func TestExists(t *testing.T) {
+	client, _ := database.CreateLocalClient(8000)
+	exist := database.TableExists(client, "test")
+	if exist {
+		t.Error("Error, it doesn't")
 	}
+}
+
+func TestCreateTable(t *testing.T) {
+	client, _ := database.CreateLocalClient(8000)
+	created := database.CreateTableIfNotExists(client, TABLE_NAME)
+	if !created {
+		t.Error("Error, not created")
+	}
+
 }
